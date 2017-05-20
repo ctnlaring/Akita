319642ef003894e599949db9161c7520e80e1d53
Collin Norwood <collinthegeek@gmail.com>
2017-05-20 18:00:53 -0500

    more testing

---
+++ out.sh
@@ -0,0 +1,12 @@
+This file will hold the output script
+My writing test!
+My writing test!
+My writing test!
+False
+False
+False
+False
+False
+False
+gi.FunctionInfo(get_active)
+gi.FunctionInfo(get_active)
