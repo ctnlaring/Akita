6f4462cddca1a0907d05929d5d66caa8599b8748
Collin Norwood <collinthegeek@gmail.com>
2017-05-20 21:45:12 -0500

    made it less terrible

--- main.py
+++ main.py
@@ -7,128 +7,73 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk as gtk
 from subprocess import call
 import os
 os.system("rm out.sh")
 os.system("echo 'echo installing' > out.sh")
-class WelcomeWindow(Gtk.Window):
+class installer(gtk.Window):
 
 	def __init__(self):
-		Gtk.Window.__init__(self, title="Unnamed thingy")
+		gtk.Window.__init__(self, title="Arch installer")
 		self.set_border_width(10)
-
-		box = Gtk.Box(spacing=6)
+		box = gtk.Box(orientation=gtk.Orientation.VERTICAL)
 		self.add(box)
-		stepsbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		box.add(stepsbox)
-
-		step1label = Gtk.Label("Step 1")
-		stepsbox.add(step1label)
-		step2label = Gtk.Label("Step 2")
-		stepsbox.add(step2label)
-		step3label = Gtk.Label("Step 3")
-		stepsbox.add(step3label)
-		step4label = Gtk.Label("Step 4")
-		stepsbox.add(step4label)
-		step5label = Gtk.Label("Step 5")
-		stepsbox.add(step5label)
-
-		mainbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		box.add(mainbox)
-
-		welcomelabel = Gtk.Label("Welcome to this unnamed thingy. Hopefully this will let you install arch linux at some point. As you chose options and click next on each page it will basically make a script based on what options you selected.")
-		mainbox.add(welcomelabel)
-		testcheck = Gtk.CheckButton("Install Firefox?")
-		mainbox.add(testcheck)
-		radio1 = Gtk.RadioButton("radio1")
-		radio2 = Gtk.RadioButton(group=radio1, label="radio2")
-		mainbox.pack_start(radio1, False, False, padding=5)
-		mainbox.pack_start(radio2, False, False, padding=5)
-
-		buttonbox = Gtk.Box(spacing=6)
-		mainbox.pack_end(buttonbox, False, False, padding=0)
-
-		button = Gtk.Button.new_with_label("Exit")
+		mainbook = gtk.Notebook()
+		box.pack_start(mainbook, True, True, padding=0)
+		
+		
+		buttonbox = gtk.Box()
+		box.pack_end(buttonbox, False, False, padding=5)
+		
+		button = gtk.Button.new_with_label("Exit")
 		button.connect("clicked", self.quitbutton)
-		buttonbox.pack_start(button, False, False, padding=0)
+		buttonbox.pack_start(button, False, False, padding=5)
 
-		button = Gtk.Button.new_with_label("Next")
-		button.connect("clicked", self.nextbutton, testcheck.get_active)
-		buttonbox.pack_end(button, False, False, padding=0)
+		button = gtk.Button.new_with_label("Next")
+		button.connect("clicked", self.nextbutton)
+		buttonbox.pack_end(button, False, False, padding=5)
 
-		button = Gtk.Button.new_with_label("Back")
+		button = gtk.Button.new_with_label("Back")
 		button.connect("clicked", self.backbutton)
-		buttonbox.pack_end(button, False, False, padding=0)
+		buttonbox.pack_end(button, False, False, padding=5)
+		button.set_sensitive(False)
 
-	def nextbutton(self, button, checked):
-		print(checked())
-		f = open("out.sh", "a");
-		if checked() == True:
-			f.write("sudo dnf install firefox\n")
-		else:
-			f.write("echo 'skipping firefox'\n")
-		f.close()
-		win = SecondWindow()
-		Gtk.Window.resize(win,800,500)
-		win.connect("delete-event", Gtk.main_quit)
-		win.show_all()
-		Gtk.main()
 
-	def backbutton(self, button, back):
+
+		#Page 1
+		page1 = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = gtk.Label("Welcome to this unnamed thingy. Hopefully this will let you install arch linux at some point. As you chose options and click next on each page it will basically make a script based on what options you selected.")
+		page1.add(welcomelabel)
+
+		#Page 2
+		page2 = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = gtk.Label("hello again")
+		page2.add(welcomelabel)
+		
+
+		#Tabs
+		tab1label = gtk.Label("tab1")
+		tab2label = gtk.Label("tab2")
+		tab3label = gtk.Label("tab3")
+		tab4label = gtk.Label("tab4")
+		tab5label = gtk.Label("tab5")
+		mainbook.append_page(page1, tab1label)
+		mainbook.append_page(page2, tab2label)
+
+
+	def nextbutton(self, button):
+		print("running")
+		os.system("gnome-terminal -x sh -c 'bash out.sh; exec bash'")
+
+	def backbutton(self, button):
  		print("back")
 		
 	def quitbutton(self, button):
-		print("exit")
-		Gtk.main_quit()
+		gtk.main_quit()
 
-class SecondWindow(Gtk.Window):
-
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Unnamed thingy")
-		self.set_border_width(10)
-
-		box = Gtk.Box(spacing=6)
-		self.add(box)
-		stepsbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		box.add(stepsbox)
-
-		step1label = Gtk.Label("Step 1")
-		stepsbox.add(step1label)
-		step2label = Gtk.Label("Step 2")
-		stepsbox.add(step2label)
-		step3label = Gtk.Label("Step 3")
-		stepsbox.add(step3label)
-		step4label = Gtk.Label("Step 4")
-		stepsbox.add(step4label)
-		step5label = Gtk.Label("Step 5")
-		stepsbox.add(step5label)
-
-		mainbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		box.add(mainbox)
-
-		gobutton = Gtk.Button.new_with_label("Run it")
-		gobutton.connect("clicked", self.gobutton)
-		mainbox.add(gobutton)
-
-		buttonbox = Gtk.Box(spacing=6)
-		mainbox.add(buttonbox)
-
-		button = Gtk.Button.new_with_label("Exit")
-		button.connect("clicked", self.quitbutton)
-		buttonbox.pack_start(button, True, True, padding=0)
-
-
-	def quitbutton(self, button):
-		print("exit")
-		Gtk.main_quit()
-
-	def gobutton(self, button):
-		print("running")
-		os.system("gnome-terminal -x sh -c 'bash out.sh; exec bash'")
-	
-win = WelcomeWindow()
-Gtk.Window.resize(win,800,500)
-win.connect("delete-event", Gtk.main_quit)
+win = installer()
+gtk.Window.resize(win,800,800)
+win.connect("delete-event", gtk.main_quit)
 win.show_all()
-Gtk.main()
+gtk.main()
--- out.sh
+++ out.sh
@@ -1,2 +1,1 @@
 echo installing
-sudo dnf install firefox
