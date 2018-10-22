4afe3308ca1c2f0e7727b79d2c74311451a6a635
Collin Norwood <cnorwood@unknown002608E8939E.attlocal.net>
2018-10-22 10:37:51 -0500

    Added more half-baked features

--- main.py
+++ main.py
@@ -4,59 +4,57 @@
 # Akita
 # Copyright (C) 2017 Collin Norwood <cnorwood7641@stu.neisd.net>
 
-import gi
+import gi, os
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
 gi.require_version('Vte', '2.91')
-from gi.repository import Vte as vte
+from gi.repository import Gtk, Vte, GdkPixbuf, GLib
 from subprocess import call
-import os
 os.system("rm out.sh")
 os.system("echo 'echo installing' > out.sh")
 print("Welcome to Akita")
 print("This terminal will show some debug output")
 
-class installer(gtk.Window):
+class installer(Gtk.Window):
 
 	def __init__(self):
 
-		gtk.Window.__init__(self, title="Akita")
+		Gtk.Window.__init__(self, title="Akita")
 		self.set_border_width(10)
-		self.set_icon_from_file("icon.png")
+		self.set_icon_from_file("data/icon.png")
 		self.set_wmclass ("Akita", "Akita")
-		box = gtk.Box(orientation=gtk.Orientation.VERTICAL)
+		box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 		self.add(box)
 		global mainbook
-		mainbook = gtk.Notebook()
+		mainbook = Gtk.Notebook()
 		box.pack_start(mainbook, True, True, padding=0)
 
 
-		buttonbox = gtk.Box()
+		buttonbox = Gtk.Box()
 		box.pack_end(buttonbox, False, False, padding=5)
 
-		button = gtk.Button.new_with_label("Exit")
+		button = Gtk.Button.new_with_label("Exit")
 		button.connect("clicked", self.quitbutton)
 		buttonbox.pack_start(button, False, False, padding=5)
 		
-		button = gtk.Button.new_with_label("Next")
+		button = Gtk.Button.new_with_label("Next")
 		button.connect("clicked", self.nextbutton)
 		buttonbox.pack_end(button, False, False, padding=5)
 
-		button = gtk.Button.new_with_label("Back")
+		button = Gtk.Button.new_with_label("Back")
 		button.connect("clicked", self.backbutton)
 		buttonbox.pack_end(button, False, False, padding=5)
 
 
-		welcomepage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		topbox = gtk.Box()
+		welcomepage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		topbox = Gtk.Box()
 		welcomepage.add(topbox)
-		icon = gtk.Image()
-		icon.set_from_file("icon.png")
+		icon = Gtk.Image()
+		icon.set_from_file("data/icon.png")
 		topbox.add(icon)
-		welcomelabel = gtk.Label()
+		welcomelabel = Gtk.Label()
 		welcomelabel.set_markup("<span font_size='x-large'><b>   Welcome to Akita</b> - v0.6</span>")
 		topbox.add(welcomelabel)
-		label = gtk.Label(margin=8)
+		label = Gtk.Label(margin=8)
 		label.set_markup("<span font_size='large'><b>\n\nThis is pre-release software. It's not ready for use on systems with installations you care about. For now it won't run 'out.sh' automatically. Do so manually only after you've verified it's correct. I'm not responsible if it breaks anything\n\n</b></span>")
 		label.set_line_wrap(True)
 		welcomepage.add(label)
@@ -69,32 +67,32 @@ class installer(gtk.Window):
 			if cards.read().strip() == "up":
 				print("Found a working connection!")
 				print("\n")
-				label = gtk.Label()
+				label = Gtk.Label()
 				label.set_markup("You're connected to the internet. Hooray.")
 				break
 			if cards.read().strip() != "up":
 				print("it appears to be down :(")
-				label = gtk.Label()
+				label = Gtk.Label()
 				label.set_markup("<b>You don't appear to be connected to the internet.</b>")
 		welcomepage.add(label)
 		cards.close()
-		helpbox = gtk.Box()
+		helpbox = Gtk.Box()
 		welcomepage.pack_end(helpbox, True, False, padding=6)
-		help = gtk.LinkButton("https://github.com/collinthegeek/Akita/issues/new", label="Help!")
+		help = Gtk.LinkButton("https://github.com/collinthegeek/Akita/issues/new", label="Help!")
 		helpbox.pack_start(help, True, False, padding=6)
-		github = gtk.LinkButton("https://github.com/collinthegeek/Akita", label="Github")
+		github = Gtk.LinkButton("https://github.com/collinthegeek/Akita", label="Github")
 		helpbox.pack_end(github, True, False, padding=6)
 
 
 
-		partitionpage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		welcomelabel = gtk.Label(margin=6)
+		partitionpage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = Gtk.Label(margin=6)
 		welcomelabel.set_markup("<span font_size='x-large'><b>Choose where to install:</b></span>")
 		partitionpage.add(welcomelabel)
-		partbox = gtk.Box()
+		partbox = Gtk.Box()
 		partitionpage.pack_end(partbox, True, True, padding=5)
-		drivebox = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		schemebox = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
+		drivebox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		schemebox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 		partbox.add(drivebox)
 		partbox.add(schemebox)
 
@@ -108,27 +106,25 @@ class installer(gtk.Window):
 				if runs == 1:
 					runs = 2
 					global firstdrive
-					firstdrive = gtk.RadioButton(margin=5,label = "/dev/" + drive)
+					firstdrive = Gtk.RadioButton(margin=5,label = "/dev/" + drive)
 					drivebox.add(firstdrive)
 				else:
-					parts["/dev/" + drive] = gtk.RadioButton(margin=5, group=firstdrive, label = "/dev/" + drive)
+					parts["/dev/" + drive] = Gtk.RadioButton(margin=5, group=firstdrive, label = "/dev/" + drive)
 					drivebox.add(parts["/dev/" + drive])
 
 
 		global ext3
-		ext3 = gtk.RadioButton(margin=6, label="ext3")
+		ext3 = Gtk.RadioButton(margin=6, label="ext3")
 		schemebox.add(ext3)
 		global ext4
-		ext4 = gtk.RadioButton(margin=5, group=ext3, label="ext4")
+		ext4 = Gtk.RadioButton(margin=5, group=ext3, label="ext4")
 		schemebox.add(ext4)
 
 
 
-		timezonepage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		welcomelabel = gtk.Label(margin_right=500, margin_top=12, label="Pick a time zone:")
-		timezonepage.add(welcomelabel)
+		timezonepage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)	
 		global zone
-		zone = gtk.ComboBoxText(margin_right=500, margin_left=12, margin_top=12)
+		"""zone = Gtk.ComboBoxText(margin_right=500, margin_left=12, margin_top=12)
 		excludedirs = set(['posix', 'right', 'Etc'])
 		excludefiles = set(['MET', 'W-SU', 'zone.tab', 'PRC', 'posixrules', 'iso3166.tab', 'zone1970.tab'])
 		for root, directories, filenames in os.walk('/usr/share/zoneinfo/', topdown=True):
@@ -137,35 +133,41 @@ class installer(gtk.Window):
 			for filename in filenames:
 				zone.append_text(os.path.join(root[20:], filename))
 		zone.set_active(0)
-		timezonepage.add(zone)
-		namelabel = gtk.Label(margin_right=500, margin_top=24, label="Enter a hostname:")
+		timezonepage.add(zone)"""
+		
+		
+		pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename="data/map.png", width=550, height=550, preserve_aspect_ratio=True)
+		zonemap = Gtk.Image.new_from_pixbuf(pixbuf)
+		timezonepage.add(zonemap)
+		
+		namelabel = Gtk.Label(margin_right=500, margin_top=24, label="Enter a hostname:")
 		timezonepage.add(namelabel)
 		global hostname
-		hostname = gtk.Entry(margin_right=500, margin_left=12, margin_top=12)
+		hostname = Gtk.Entry(margin_right=500, margin_left=12, margin_top=12)
 		hostname.set_placeholder_text("arch")
 		timezonepage.add(hostname)
 
 
 
-		userpage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		welcomelabel = gtk.Label(margin=6)
+		userpage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = Gtk.Label(margin=6)
 		welcomelabel.set_markup("<span font_size='x-large'><b>User setup</b></span>")
 		userpage.add(welcomelabel)
-		info = gtk.Label(margin_right=24, margin_left=24, margin_top=12, label="Root password:")
+		info = Gtk.Label(margin_right=24, margin_left=24, margin_top=12, label="Root password:")
 		userpage.add(info)
 		global rootpassword
-		rootpassword = gtk.Entry(margin_right=24, margin_left=24, margin_top=12)
+		rootpassword = Gtk.Entry(margin_right=24, margin_left=24, margin_top=12)
 		userpage.add(rootpassword)
-		info = gtk.Label(margin_right=24, margin_left=24, margin_top=12, label="Administrator:")
+		info = Gtk.Label(margin_right=24, margin_left=24, margin_top=12, label="Administrator:")
 		userpage.add(info)
-		userbox = gtk.Box()
+		userbox = Gtk.Box()
 		userpage.add(userbox)
 		global username
-		username = gtk.Entry(margin_right=12, margin_left=12, margin_top=12)
+		username = Gtk.Entry(margin_right=12, margin_left=12, margin_top=12)
 		username.set_placeholder_text("username")
 		userbox.pack_start(username, True, True, padding=0)
 		global password
-		password = gtk.Entry(margin_right=12, margin_left=12, margin_top=12)
+		password = Gtk.Entry(margin_right=12, margin_left=12, margin_top=12)
 		password.set_placeholder_text("password")
 		userbox.pack_end(password, True, True, padding=0)
 
@@ -173,121 +175,121 @@ class installer(gtk.Window):
 
 		global media
 		media = {
-		'vlc': gtk.CheckButton("VLC")
+		'vlc': Gtk.CheckButton("VLC")
 		}
 
 		global internet
 		internet = {
-		'firefox': gtk.CheckButton("Firefox"),
-		'tor': gtk.CheckButton("Tor"),
-		'chrome': gtk.CheckButton("Chrome"),
-		'pidgin': gtk.CheckButton("Pidgin"),
-		'hangouts': gtk.CheckButton("Hangouts"),
-		'dropbox': gtk.CheckButton("Dropbox"),
-		'drive': gtk.CheckButton("Google Drive"),
-		'thunderbird': gtk.CheckButton("Thunderbird"),
-		'polari': gtk.CheckButton("Polari"),
-		'transmission': gtk.CheckButton("Transmission")
+		'firefox': Gtk.CheckButton("Firefox"),
+		'tor': Gtk.CheckButton("Tor"),
+		'chrome': Gtk.CheckButton("Chrome"),
+		'pidgin': Gtk.CheckButton("Pidgin"),
+		'hangouts': Gtk.CheckButton("Hangouts"),
+		'dropbox': Gtk.CheckButton("Dropbox"),
+		'drive': Gtk.CheckButton("Google Drive"),
+		'thunderbird': Gtk.CheckButton("Thunderbird"),
+		'polari': Gtk.CheckButton("Polari"),
+		'transmission': Gtk.CheckButton("Transmission")
 		}
 		global productivity
 		productivity = {
-		'libreoffice': gtk.CheckButton("LibreOffice"),
-		'abiword': gtk.CheckButton("AbiWord"),
-		'lyx': gtk.CheckButton("LyX"),
+		'libreoffice': Gtk.CheckButton("LibreOffice"),
+		'abiword': Gtk.CheckButton("AbiWord"),
+		'lyx': Gtk.CheckButton("LyX"),
 		}
 		global games
 		games = {
-		'supertuxkart': gtk.CheckButton("SuperTuxKart"),
-		'frozen-bubble': gtk.CheckButton("Fozen Bubble"),
-		'battle-for-wesnoth': gtk.CheckButton("Battle for Wesnoth")
+		'supertuxkart': Gtk.CheckButton("SuperTuxKart"),
+		'frozen-bubble': Gtk.CheckButton("Fozen Bubble"),
+		'battle-for-wesnoth': Gtk.CheckButton("Battle for Wesnoth")
 		}
 		global graphics
 		graphics = {
-		'gimp': gtk.CheckButton("Gimp")
+		'gimp': Gtk.CheckButton("Gimp")
 		}
 		global development
 		development = {
-		'emacs': gtk.CheckButton("Emacs"),
-		'nano': gtk.CheckButton("Nano")
+		'emacs': Gtk.CheckButton("Emacs"),
+		'nano': Gtk.CheckButton("Nano")
 		}
 		global education
 		education = {
-		'celestia': gtk.CheckButton("Celestia Space Simulator")
+		'celestia': Gtk.CheckButton("Celestia Space Simulator")
 		}
 		global utilities
 		utilities = {
-		'synapse': gtk.CheckButton("Synapse"),
-		'autokey': gtk.CheckButton("Autokey"),
-		'shotwell': gtk.CheckButton("Shotwell"),
-		'wine': gtk.CheckButton("Wine"),
-		'virtualbox': gtk.CheckButton("Virtualbox")
+		'synapse': Gtk.CheckButton("Synapse"),
+		'autokey': Gtk.CheckButton("Autokey"),
+		'shotwell': Gtk.CheckButton("Shotwell"),
+		'wine': Gtk.CheckButton("Wine"),
+		'virtualbox': Gtk.CheckButton("Virtualbox")
 		}
 
 
 
-		softwarepage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
+		softwarepage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 		global packages
 		packages = [];
-		softwarebook = gtk.Notebook()
-		softwarebook.set_tab_pos(gtk.PositionType.LEFT)
+		softwarebook = Gtk.Notebook()
+		softwarebook.set_tab_pos(Gtk.PositionType.LEFT)
 		softwarepage.pack_end(softwarebook, True, True, padding=5)
 
-		mediatab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Media")
+		mediatab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Media")
 		mediatab.add(tablabel)
 		for app in media:
 			mediatab.add(media[app])
 
-		nettab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Internet")
+		nettab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Internet")
 		nettab.add(tablabel)
 		for app in internet:
 			nettab.add(internet[app])
 
-		productivitytab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Productivity")
+		productivitytab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Productivity")
 		productivitytab.add(tablabel)
 		for app in productivity:
 			productivitytab.add(productivity[app])
 
-		gamestab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Games")
+		gamestab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Games")
 		gamestab.add(tablabel)
 		for app in games:
 			gamestab.add(games[app])
 
-		graphicstab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Graphics")
+		graphicstab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Graphics")
 		graphicstab.add(tablabel)
 		for app in graphics:
 			graphicstab.add(graphics[app])
 
-		devtoolstab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Developer Tools")
+		devtoolstab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Developer Tools")
 		devtoolstab.add(tablabel)
 		for app in development:
 			devtoolstab.add(development[app])
 
-		educationtab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Education")
+		educationtab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Education")
 		educationtab.add(tablabel)
 		for app in education:
 			educationtab.add(education[app])
 
-		utilitiestab = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		tablabel = gtk.Label(margin_top=6, label="Utilites")
+		utilitiestab = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		tablabel = Gtk.Label(margin_top=6, label="Utilites")
 		utilitiestab.add(tablabel)
 		for app in utilities:
 			utilitiestab.add(utilities[app])
 
-		media = gtk.Label("Media")
-		net = gtk.Label("Internet")
-		productivity = gtk.Label("Productivity")
-		games = gtk.Label("Games")
-		graphics= gtk.Label("Graphics")
-		devtools = gtk.Label("Dev Tools")
-		education = gtk.Label("Education")
-		utilites = gtk.Label("Utilites")
+		media = Gtk.Label("Media")
+		net = Gtk.Label("Internet")
+		productivity = Gtk.Label("Productivity")
+		games = Gtk.Label("Games")
+		graphics= Gtk.Label("Graphics")
+		devtools = Gtk.Label("Dev Tools")
+		education = Gtk.Label("Education")
+		utilites = Gtk.Label("Utilites")
 		softwarebook.append_page(mediatab, media)
 		softwarebook.append_page(nettab, net)
 		softwarebook.append_page(productivitytab, productivity)
@@ -299,33 +301,37 @@ class installer(gtk.Window):
 
 
 
-		summarypage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		welcomelabel = gtk.Label(margin=6)
+		summarypage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = Gtk.Label(margin=6)
 		welcomelabel.set_markup("<span font_size='x-large'><b>Summary:</b></span>")
 		summarypage.add(welcomelabel)
+		summarylabel = Gtk.Label()
+		summarylabel.set_markup("Here's what I'm going to do:\n\nMake an XXX filesystem on XXX\nPut Arch on it\nSet up your hostname, passwords, user, GRUB, etc.\nInstall the following applications:\n")
+		summarypage.add(summarylabel)
 
 
 
-		finalpage = gtk.Box(orientation=gtk.Orientation.VERTICAL, spacing=6)
-		welcomelabel = gtk.Label()
+		finalpage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		welcomelabel = Gtk.Label()
 		welcomelabel.set_markup("I'm now going to attempt to generate an install script based on the options you selected.\nI won't run it automatically for now. Do so manually only after <b>careful</b> review.")
 		finalpage.add(welcomelabel)
-		gobutton = gtk.Button("GO!")
+		gobutton = Gtk.Button("GO!")
 		gobutton.connect("clicked", self.write)
 		finalpage.pack_end(gobutton, False, False, padding=5)
-		terminal = vte.Terminal()
-		terminal.connect ("child-exited", lambda term: gtk.main_quit())
-		terminal.feed_child("echo hi", True)
+		terminal = Vte.Terminal()
+		terminal.spawn_sync(Vte.PtyFlags.DEFAULT, ".", ["/bin/bash"], [], GLib.SpawnFlags.DO_NOT_REAP_CHILD, None, None)
+		command="hi"
+		#terminal.feed_child(command, len(command))
 		finalpage.add(terminal)
 
 		#Tabs
-		welcome = gtk.Label("Welcome")
-		disks = gtk.Label("Disks")
-		timezone = gtk.Label("Time Zone")
-		users = gtk.Label("Users")
-		software = gtk.Label("Software")
-		summary = gtk.Label("Summary")
-		finish = gtk.Label("Finish")
+		welcome = Gtk.Label("Welcome")
+		disks = Gtk.Label("Disks")
+		timezone = Gtk.Label("Time Zone")
+		users = Gtk.Label("Users")
+		software = Gtk.Label("Software")
+		summary = Gtk.Label("Summary")
+		finish = Gtk.Label("Finish")
 		mainbook.append_page(welcomepage, welcome)
 		mainbook.append_page(partitionpage, disks)
 		mainbook.append_page(timezonepage, timezone)
@@ -452,16 +458,16 @@ class installer(gtk.Window):
 		print("\n\n")
 		print("Now open 'out.sh' in your favorite editor and make sure it's correct before running it. AS ROOT!")
 		print("You've been warned")
-		gtk.main_quit()
+		Gtk.main_quit()
 
 	def backbutton(self, button):
 		mainbook.prev_page()
 
 	def quitbutton(self, button):
-		gtk.main_quit()
+		Gtk.main_quit()
 
 win = installer()
-gtk.Window.resize(win,800,500)
-win.connect("delete-event", gtk.main_quit)
+Gtk.Window.resize(win,800,500)
+win.connect("delete-event", Gtk.main_quit)
 win.show_all()
-gtk.main()
+Gtk.main()
